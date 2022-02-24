16646f642b1879c6465701a0cf17c3e018b8343d
Eduardo Alexi Costella Espinoza <eduardoalexicostellaespinoza@MacBook-Pro-de-Eduardo.local>
2022-02-24 09:50:24 -0300

    Version 0.0.1 consuming our npm eace-product-cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import { ProductCard, ProductImage, ProductTitle, ProductButtons } from 'eace-product-card';
 import './App.css';
+
+export const product = {
+  id: '2',
+  title: 'Coffee Mug - Sin Imnagen',
+  img : './coffee-mug.png'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <ProductCard
+          key = { product.id } 
+          product = { product }
+          initialValues = {{  
+              count: 4,
+              maxCount: 10
+          }}
+       >
+          {
+              ( { reset, count, increaseBy, isMaxCountReached, maxCount } ) => (
+                      <>
+                          <ProductImage />
+                          <ProductTitle />
+                          <ProductButtons />
+                      </>
+                    )
+          }
+      </ProductCard>
     </div>
   );
 }
